0a7b8d07aa51bcef449210ea393017343b2a1597
Enrico Graziani <enrico@carbonre.com>
2023-02-26 18:15:03 +0000

    Revert "Removed TailwindCSS"
    This reverts commit 78737324eb2e3d2a3e1cb3b848831decae622719.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import FirebaseComponents from '@/components/firebase-components';
 import { firebaseConfig } from '@/firebase.config';
+import '@/styles/globals.css'
 import { FirebaseAppProvider } from 'reactfire';
 
 export default function App({ Component, pageProps }) {
