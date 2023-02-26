78737324eb2e3d2a3e1cb3b848831decae622719
Enrico Graziani <enrico@carbonre.com>
2023-02-26 18:11:07 +0000

    Removed TailwindCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import FirebaseComponents from '@/components/firebase-components';
 import { firebaseConfig } from '@/firebase.config';
-import '@/styles/globals.css'
 import { FirebaseAppProvider } from 'reactfire';
 
 export default function App({ Component, pageProps }) {
