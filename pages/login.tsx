d10c1ac9f800ef770df0f47c8435e5202e3e9bf9
Enrico Graziani <enrico@carbonre.com>
2023-02-27 18:13:24 +0000

    Remove Tailwind again :D

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,49 @@
-import Signin from '@/components/signin';
-import { AuthWrapper } from '@/components/auth-wrapper';
 import { Loading } from '@/components/loading';
-import { SuspenseWithPerf } from 'reactfire';
+import { SuspenseWithPerf, useAuth, useSigninCheck } from 'reactfire';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 
-function RedirectToProfile() {
+const googleAuthProvider = new GoogleAuthProvider();
+
+export function Signin() {
+  const auth = useAuth();
+
+  function handleFormSubmit(event) {
+    event.preventDefault(); // prevent the form from actually submitting
+    const email = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
+    // do something with the username and password, e.g. send them to a server using an AJAX request
+    signInWithEmailAndPassword(auth, email, password)
+  }
+
+  return (
+    <main>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" name="email" required /><br /><br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required /><br /><br />
+        <button type="submit">Login</button>
+      </form>
+      <button onClick={() => signInWithPopup(auth, googleAuthProvider)}>Sign in with Google</button>
+    </main>
+  );
+}
+
+export const AuthWrapper = ({ children, fallback }: React.PropsWithChildren<{ fallback: JSX.Element }>): JSX.Element => {
+  const { data: signInCheckResult, status } = useSigninCheck();
+  
+  if (!children)  throw new Error('Children must be provided');
+  if (signInCheckResult?.signedIn === true) return children as JSX.Element;
+  if (status === 'loading') return <>Loading</>
+  return fallback;
+};
+
+function RedirectToHome() {
   const router = useRouter();
   useEffect(() => {
-    router.push("/profile");
+    router.push("/");
   }, [])
   return undefined;
 }
@@ -17,7 +52,7 @@ export default function Login() {
   return (
     <SuspenseWithPerf traceId={'firebase-user-wait'} fallback={<Loading />}>
       <AuthWrapper fallback={<Signin />}>
-        <RedirectToProfile />
+        <RedirectToHome />
       </AuthWrapper>
     </SuspenseWithPerf>
   );
