3f593497b257b9aaae0567989cbffc86a8170aeb
Enrico Graziani <enrico@carbonre.com>
2023-02-27 18:24:41 +0000

    Home page show user info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
+import { signOut } from "@firebase/auth";
 import Link from "next/link";
+import { useAuth, useUser } from "reactfire";
 
 export default function Home() {
+  const auth = useAuth();
+  const { data: user }  = useUser();
+  
   return (
     <main>
       <h1>Home</h1>
-      <ul>
-        <li><Link href="/login">Login</Link></li>
-      </ul>
+      {user ? (
+        <>
+          Logged in as: {user.displayName}<br />
+          Email: {user.email}<br />
+          <button onClick={() => signOut(auth)}>Sign out</button>
+        </>
+      ) : (
+        <>
+          <Link href="/login">Login</Link><br />
+        </>
+      )}
     </main>
   )
 }
