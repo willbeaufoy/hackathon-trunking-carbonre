27c21e8789d3b46653513cda2aac5ec57d8f21c2
Enrico Graziani <enrico@carbonre.com>
2023-03-15 22:33:57 +0000

    Moved user details in layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
+import { useUser } from "reactfire";
 
 export default function Layout({ children }) {
+    const { data: user } = useUser();
+
     return (
         <>
             <nav>
                 <ul>
-                    <li><Link href="/sign-in">Sign in</Link></li>
-                    <li><Link href="/sign-up">Sign up</Link></li>
+
+                    {user?.email ? (
+                        <li>Hello {user.email}</li>
+                    ) : (
+                        <>
+                            <li><Link href="/sign-in">Sign in</Link></li>
+                            <li><Link href="/sign-up">Sign up</Link></li>
+                        </>
+                    )}
                 </ul>
             </nav>
             {children}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,6 @@
-import { useUser } from "reactfire"
-
 export default function Home() {
-  const {data: user} = useUser();
-
   return (
     <main>
-      {user?.email && (<>Hello {user.email}</>)}
       <h1>Home</h1>
     </main>
   )
