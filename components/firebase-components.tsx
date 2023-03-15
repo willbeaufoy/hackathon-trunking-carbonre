21fe29135ee68d51f7b7f87cd8a3af643b9dcfa6
Enrico Graziani <enrico@carbonre.com>
2023-03-15 22:28:58 +0000

    Get rid of stuff I don't know why is there

--- components/firebase-components.tsx
+++ components/firebase-components.tsx
@@ -4,14 +4,9 @@ import { useEffect } from 'react';
 import { AuthProvider, useFirebaseApp } from 'reactfire';
 
 // TODO AppCheck
+// TODO Analytics
+// TODO Performance
 // TODO SSR auth
-
-// const app = initializeApp(firebaseConfig);
-import { getAnalytics } from "firebase/analytics";
-// const analytics = getAnalytics(app);
-
-const isSSR = typeof window === "undefined"
-const isCSR = !isSSR
 
 export default function FirebaseComponents({ children }) {
     const app = useFirebaseApp();
@@ -19,28 +14,13 @@ export default function FirebaseComponents({ children }) {
 
     useEffect(() => {
         if (process.env.NEXT_PUBLIC_EMULATOR === 'true') {
-            // Set up emulators
             connectAuthEmulator(auth, 'http://127.0.0.1:9099', {disableWarnings: true});
         }
-
-        if (isCSR) getAnalytics(app);
     }, [app])
-
-    // useInitPerformance(
-    //     async (firebaseApp) => {
-    //         const { getPerformance } = await import('firebase/performance');
-    //         return getPerformance(firebaseApp);
-    //     },
-    //     { suspense: false } // false because we don't want to stop render while we wait for perf
-    // );
 
     return (
         <AuthProvider sdk={auth}>
             {children}
         </AuthProvider>
     );
-}
-
-function useInitPerformance(arg0: (firebaseApp: any) => Promise<import("firebase/performance").FirebasePerformance>, arg1: { suspense: boolean; }) {
-    throw new Error('Function not implemented.');
 }
