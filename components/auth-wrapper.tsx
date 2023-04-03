2dcc493e29443c82b8410d5337bfce69e0e68956
Enrico Graziani <enrico@carbonre.com>
2023-04-03 21:03:56 +0100

    Refactor to useSigninCheck

--- components/auth-wrapper.tsx
+++ components/auth-wrapper.tsx
@@ -1,16 +1,12 @@
 import { useRouter } from "next/router";
-import { useUser } from "reactfire";
+import { useSigninCheck } from "reactfire";
 
 
 export const AuthWrapper = ({ children, fallback }: React.PropsWithChildren<{ fallback: JSX.Element }>): JSX.Element => {
-  const { data: user, status } = useUser();
+  const { data: {user} } = useSigninCheck();
   const router = useRouter();
 
-  console.log({user, verified: user?.emailVerified})
-
-  if (!children) throw new Error('Children must be provided');
   if (!user) return fallback;
   if (!user.emailVerified) router.push('/validate-email');
-  if (status === 'loading') return <>Loading</>
   return children as JSX.Element;
 };
