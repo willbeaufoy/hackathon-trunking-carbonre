88d17e2b51b6f00ee5e687ccd6da8214d2a272f8
Enrico Graziani <enrico@carbonre.com>
2023-04-13 00:33:07 +0100

    Savind Daily Outcomes too

--- e2e/today.spec.ts
+++ e2e/today.spec.ts
@@ -1,4 +1,6 @@
 import { expect, test } from '@playwright/test';
+import { set } from 'firebase/database';
+import { setTimeout } from 'timers/promises';
 import { getRandomChars } from './tools';
 import { signupAndLogin } from './user-management';
 
@@ -24,19 +26,24 @@ test('create weekly outcomes and persist them', async ({ page }) => {
 	{
 		const outcomes = await page
 			.getByRole('region', { name: 'Weekly Outcomes' })
-			.getByRole('listitem')
-			.all();
-		expect(outcomes.length).toBe(3);
+			.getByRole('listitem');
 
-		for await (const [index, outcome] of outcomes.entries()) {
-			await outcome
+		for await (const [
+			index,
+			{ hotSpot, outcome },
+		] of outcomesFixture.entries()) {
+			await outcomes
+				.nth(index)
 				.getByRole('combobox', { name: 'Hot spot' })
-				.selectOption(outcomesFixture[index].hotSpot);
-			await outcome
+				.selectOption(hotSpot);
+			await outcomes
+				.nth(index)
 				.getByPlaceholder('Enter your weekly outcome')
-				.fill(outcomesFixture[index].outcome);
-			await outcome.getByRole('button', { name: 'Save' }).click();
+				.fill(outcome);
+			await outcomes.nth(index).getByRole('button', { name: 'Save' }).click();
 		}
+
+		expect(await outcomes.all()).toHaveLength(3);
 	}
 
 	await page.reload();
@@ -44,29 +51,104 @@ test('create weekly outcomes and persist them', async ({ page }) => {
 	{
 		await expect(page.getByRole('heading', { name: 'Today' })).toBeVisible();
 
-		const outcomes = await page
+		const outcomes = page
 			.getByRole('region', { name: 'Weekly Outcomes' })
-			.getByRole('listitem')
-			.all();
-		expect(outcomes.length).toBe(3);
+			.getByRole('listitem');
 
-		for await (const [index, outcome] of outcomes.entries()) {
+		for await (const [
+			index,
+			{ hotSpot, outcome },
+		] of outcomesFixture.entries()) {
 			expect(
-				await outcome.getByRole('combobox', { name: 'Hot spot' }).inputValue(),
-			).toEqual(outcomesFixture[index].hotSpot);
+				await outcomes
+					.nth(index)
+					.getByRole('combobox', { name: 'Hot spot' })
+					.inputValue(),
+			).toEqual(hotSpot);
 
 			expect(
-				await outcome
+				await outcomes
+					.nth(index)
 					.getByPlaceholder('Enter your weekly outcome')
 					.inputValue(),
-			).toEqual(outcomesFixture[index].outcome);
+			).toEqual(outcome);
 		}
+
+		expect(await outcomes.all()).toHaveLength(3);
 	}
 });
 
 // AS A user
 // I WANT TO be able to create a 3 daily outcomes
 // SO THAT I can plan my day
+
+test('create daily outcomes and persist them', async ({ page }) => {
+	const outcomesFixture = [
+		{ hotSpot: 'Emotions', outcome: 'I am a Emotions daily outcome' },
+		{ hotSpot: 'Career', outcome: 'I am a Career daily outcome' },
+		{ hotSpot: 'Fun', outcome: 'I am a Fun daily outcome' },
+	];
+
+	const myEmail = `test-${getRandomChars()}@test.com`;
+	const myPassword = 'password';
+
+	await signupAndLogin(page, myEmail, myPassword);
+	await expect(page.getByRole('heading', { name: 'Today' })).toBeVisible();
+
+	{
+		const outcomes = await page
+			.getByRole('region', { name: 'Daily Outcomes' })
+			.getByRole('listitem');
+
+		for await (const [
+			index,
+			{ hotSpot, outcome },
+		] of outcomesFixture.entries()) {
+			await outcomes
+				.nth(index)
+				.getByRole('combobox', { name: 'Hot spot' })
+				.selectOption(hotSpot);
+			await outcomes
+				.nth(index)
+				.getByPlaceholder('Enter your daily outcome')
+				.fill(outcome);
+			await outcomes.nth(index).getByRole('button', { name: 'Save' }).click();
+		}
+
+		expect(await outcomes.all()).toHaveLength(3);
+	}
+
+	await page.reload();
+
+	{
+		await expect(page.getByRole('heading', { name: 'Today' })).toBeVisible();
+
+		const outcomes = page
+			.getByRole('region', { name: 'Daily Outcomes' })
+			.getByRole('listitem');
+
+		for await (const [
+			index,
+			{ hotSpot, outcome },
+		] of outcomesFixture.entries()) {
+			expect(
+				await outcomes
+					.nth(index)
+					.getByRole('combobox', { name: 'Hot spot' })
+					.inputValue(),
+			).toEqual(hotSpot);
+
+			expect(
+				await outcomes
+					.nth(index)
+					.getByPlaceholder('Enter your daily outcome')
+					.inputValue(),
+			).toEqual(outcome);
+		}
+
+		expect(await outcomes.all()).toHaveLength(3);
+	}
+});
 
 // AS A user
 // I WANT TO be able to create a few retro notes
--- pages/app/daily/[day].tsx
+++ pages/app/daily/[day].tsx
@@ -5,8 +5,17 @@ import { useRouter } from 'next/router';
 import { useCallback, useEffect } from 'react';
 import { useAuth, useFirestore, useFirestoreCollectionData } from 'reactfire';
 
+const HOTSPOTS = [
+	'Mind',
+	'Body',
+	'Relationships',
+	'Emotions',
+	'Career',
+	'Fun',
+] as const;
+
 interface WeeklyOutcomeData {
-	hotSpot: string;
+	hotSpot: string; // TODO: make this a union type
 	outcome: string;
 	day: string;
 }
@@ -37,9 +46,11 @@ function WeeklyOutcome({ hotSpot, outcome, handleSave }: WeeklyOutcomeProps) {
 				<option value="" disabled>
 					Select a hot spot
 				</option>
-				<option value="Mind">Mind</option>
-				<option value="Body">Body</option>
-				<option value="Relationships">Relationships</option>
+				{HOTSPOTS.map(hotSpot => (
+					<option key={hotSpot} value={hotSpot}>
+						{hotSpot}
+					</option>
+				))}
 			</select>
 			<textarea
 				className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
@@ -123,6 +134,107 @@ function WeeklyOutcomes() {
 	);
 }
 
+function DailyOutcome({ hotSpot, outcome, handleSave }: WeeklyOutcomeProps) {
+	const handleOnSubmit = event => {
+		event.preventDefault();
+		const hotSpot = event.target.elements.hotSpot.value;
+		const outcome = event.target.elements.outcome.value;
+		handleSave({ hotSpot, outcome });
+	};
+
+	return (
+		<form onSubmit={handleOnSubmit} className="space-y-2">
+			<select
+				name="hotSpot"
+				aria-label="Hot spot"
+				defaultValue={hotSpot || ''}
+				className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+			>
+				<option value="" disabled>
+					Select a hot spot
+				</option>
+				{HOTSPOTS.map(hotSpot => (
+					<option key={hotSpot} value={hotSpot}>
+						{hotSpot}
+					</option>
+				))}
+			</select>
+			<textarea
+				className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+				rows={3}
+				name="outcome"
+				placeholder="Enter your Daily outcome"
+				defaultValue={outcome || ''}
+			/>
+			<button
+				type="submit"
+				className="block w-36 rounded-full bg-green-300 bg-center py-3 text-center text-base font-normal text-black shadow-xl hover:bg-green-400 focus:ring-4 focus:ring-green-200 dark:focus:ring-green-900 sm:w-72"
+			>
+				Save
+			</button>
+			<br />
+		</form>
+	);
+}
+
+function DailyOutcomes() {
+	const router = useRouter();
+	const { day } = router.query;
+
+	const firestore = useFirestore();
+	const auth = useAuth();
+	const { uid } = auth.currentUser;
+	const dailyOutcomesCollection = collection(
+		firestore,
+		`users/${uid}/dailyOutcomes/`,
+	);
+	const { status, data: dailyOutcomes } = useFirestoreCollectionData(
+		dailyOutcomesCollection,
+		{ idField },
+	);
+
+	const saveDailyOutcome = useCallback(
+		(id: string) => (outcome: WeeklyOutcomeData) =>
+			setDoc(doc(dailyOutcomesCollection, id), outcome).catch(console.error),
+		[dailyOutcomesCollection],
+	);
+
+	useEffect(() => {
+		if (status !== 'success') return;
+		if (!dailyOutcomes || dailyOutcomes.length === 0) {
+			[...Array(3)].map((_, i) =>
+				saveDailyOutcome(`${i}`)({
+					...emptyWeeklyOutcome,
+					day: day as string,
+				}),
+			);
+		}
+	}, [status, saveDailyOutcome, day, dailyOutcomes]);
+
+	const isLoading = status === 'loading';
+	const hasDailyOutcomes = dailyOutcomes && dailyOutcomes.length > 0;
+	if (isLoading || !hasDailyOutcomes) {
+		return <p>Loading...</p>;
+	}
+
+	return (
+		<section aria-label="Daily Outcomes">
+			<h2>Daily Outcomes</h2>
+			<ul className="list-inside list-none space-y-8 pl-0 text-gray-500 dark:text-gray-400">
+				{dailyOutcomes.map(({ hotSpot, outcome, id }) => (
+					<li key={id}>
+						<DailyOutcome
+							hotSpot={hotSpot}
+							outcome={outcome}
+							handleSave={saveDailyOutcome(id)}
+						/>
+					</li>
+				))}
+			</ul>
+		</section>
+	);
+}
+
 function Page() {
 	const router = useRouter();
 	const { day } = router.query;
@@ -136,7 +248,8 @@ function Page() {
 					<h2>Sat 8th Apr</h2>
 				</section>
 				<WeeklyOutcomes />
-				<section>
+				<DailyOutcomes />
+				{/* <section>
 					<h2>Daily Outcomes</h2>
 					<ul>
 						<li>
@@ -166,7 +279,7 @@ function Page() {
 							/>
 						</li>
 					</ul>
-				</section>
+				</section> */}
 				<section>
 					<h2>Retro</h2>
 					<ul>
