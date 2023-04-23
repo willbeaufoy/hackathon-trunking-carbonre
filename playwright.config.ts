08ce9a7c143059acb7cf4c5843f3fd7bbdb85a96
Enrico Graziani <enrico@carbonre.com>
2023-04-23 14:58:28 +0100

    refactored config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices, PlaywrightTestConfig } from '@playwright/test';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: './.env.e2e' });
@@ -6,31 +6,31 @@ require('dotenv').config({ path: './.env.e2e' });
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
+let config: PlaywrightTestConfig = {
 	testDir: './e2e',
 	/* Maximum time one test can run for. */
-	timeout: 30_000,
+	timeout: 30000,
 	expect: {
 		/**
 		 * Maximum time expect() should wait for the condition to be met.
 		 * For example in `await expect(locator).toHaveText();`
 		 */
-		timeout: 3_000,
+		timeout: 3000,
 	},
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
+	// forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	// retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	// workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: 'line',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-		actionTimeout: 3_000,
+		actionTimeout: 3000,
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		baseURL: 'http://localhost:3000',
 
@@ -45,17 +45,14 @@ export default defineConfig({
 			name: 'chromium',
 			use: { ...devices['Desktop Chrome'] },
 		},
-
 		// {
 		//   name: 'firefox',
 		//   use: { ...devices['Desktop Firefox'] },
 		// },
-
 		// {
 		//   name: 'webkit',
 		//   use: { ...devices['Desktop Safari'] },
 		// },
-
 		/* Test against mobile viewports. */
 		// {
 		//   name: 'Mobile Chrome',
@@ -65,7 +62,6 @@ export default defineConfig({
 		//   name: 'Mobile Safari',
 		//   use: { ...devices['iPhone 12'] },
 		// },
-
 		/* Test against branded browsers. */
 		// {
 		//   name: 'Microsoft Edge',
@@ -79,11 +75,26 @@ export default defineConfig({
 
 	/* Folder for test artifacts such as screenshots, videos, traces, etc. */
 	// outputDir: 'test-results/',
-
 	/* Run your local dev server before starting the tests */
 	// globalSetup: require.resolve('./global-setup',
-	webServer: process.env.CI === 'true' && {
-		command: 'npm run firebase:dev',
-		port: 3000,
-	},
-});
+	/* Run your local dev server before starting the tests */
+	// webServer: {
+	//   command: 'npm run start',
+	//   port: 3000,
+	// },
+};
+
+if (process.env.CI === 'true') {
+	config = {
+		...config,
+		forbidOnly: !!process.env.CI,
+		retries: 2,
+		reporter: 'html',
+		webServer: {
+			command: 'npm run firebase:dev',
+			port: 3000,
+		},
+	};
+}
+
+export default defineConfig(config);
