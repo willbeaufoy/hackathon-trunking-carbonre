01807a5ebdcf64f9c481d8696a4c35d6d493919b
Enrico Graziani <enrico@carbonre.com>
2023-06-09 17:49:53 +0100

    send messages locally

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,11 +1,46 @@
 import Layout from '@/components/layout';
 import BumpUnauthorised from '@/components/login/bump-unauthorised';
 
+import { useState } from 'react';
+
+function Chat() {
+	const [messages, setMessages] = useState<string[]>([]);
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const messageInput = event.currentTarget.elements.namedItem(
+			'message',
+		) as HTMLInputElement;
+		const message = messageInput.value.trim();
+		if (message) {
+			setMessages(prevMessages => [...prevMessages, message]);
+			messageInput.value = '';
+		}
+	};
+
+	return (
+		<>
+			<h1>Chat</h1>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="message">Message</label>
+				<input type="text" name="message" placeholder="Message" />
+				<button type="submit">Send</button>
+			</form>
+
+			<ul>
+				{messages.map((message, index) => (
+					<li key={index}>{message}</li>
+				))}
+			</ul>
+		</>
+	);
+}
+
 export default function Page() {
 	return (
 		<BumpUnauthorised>
 			<Layout>
-				<h1>Chat</h1>
+				<Chat />
 			</Layout>
 		</BumpUnauthorised>
 	);
