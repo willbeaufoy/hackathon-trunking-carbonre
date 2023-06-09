20ac8d9a8149fb8d5495a4160f4b1a062f102296
Enrico Graziani <enrico@carbonre.com>
2023-06-09 18:00:46 +0100

    refactored login

--- e2e/chat.spec.ts
+++ e2e/chat.spec.ts
@@ -1,38 +1,29 @@
 import { expect, test } from '@playwright/test';
 import { getRandomChars } from './helpers/tools';
-import { deleteUser, validateEmail } from './helpers/user-management';
+import { deleteUser, signupAndLogin } from './helpers/user-management';
 
 const clean = (title: string) => title.replace(/[^a-zA-Z0-9]/g, '_');
 const toEmail = testInfo =>
 	`${clean(testInfo.title)}-${getRandomChars()}@test.com`;
 
-// create a playwright test
-test('chat', async ({ page }, testInfo) => {
-	// open the page
-	await page.goto('/');
-	// sign up
-	await page.getByText('Sign up').click();
-	// fill email / password
+test.beforeEach(async ({ page }, testInfo) => {
 	const myEmail = toEmail(testInfo);
-	await page.getByPlaceholder('Email').fill(myEmail);
-	await page.getByPlaceholder('Password').fill('myPassword');
-	// click the button
-	await page.getByRole('button', { name: 'Sign up' }).click();
-	// expect page has header Validate email
-	await expect(
-		page.getByRole('heading', { name: 'Validate email' }),
-	).toBeVisible();
+	await testInfo.attach('email', { body: myEmail });
 
-	await validateEmail(myEmail);
+	await signupAndLogin(page, myEmail);
+});
 
-	await page.goto('/');
-	// sign in
-	await page.getByText('I already have an account').click();
-	// fill email / password
-	await page.getByPlaceholder('Email').fill(myEmail);
-	await page.getByPlaceholder('Password').fill('myPassword');
-	// click the button
-	await page.getByRole('button', { name: 'Login' }).click();
+test.afterEach(async ({ page }, testInfo) => {
+	await page.close();
+	const myEmail = testInfo.attachments
+		.find(({ name }) => name === 'email')
+		.body.toString();
+
+	await deleteUser(myEmail);
+});
+
+// create a playwright test
+test('chat', async ({ page }) => {
 	// expect page has header Chat
 	await expect(page.getByRole('heading', { name: 'Chat' })).toBeVisible();
 
@@ -45,7 +36,4 @@ test('chat', async ({ page }, testInfo) => {
 	await page.getByPlaceholder('Message').fill('My second message');
 	await page.getByRole('button', { name: 'Send' }).click();
 	await expect(page.getByText('My second message')).toBeVisible();
-
-	// delete the user
-	await deleteUser(myEmail);
 });
