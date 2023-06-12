138cd0dd14b8bf527e4365ccceb6b499eaf7c600
Enrico Graziani <enrico@carbonre.com>
2023-06-12 11:13:46 +0100

    test can run 2 users/browsers

--- e2e/chat.spec.ts
+++ e2e/chat.spec.ts
@@ -18,7 +18,6 @@ test.afterEach(async ({ page }, testInfo) => {
 	await deleteUser(myEmail);
 });
 
-// create a playwright test
 test('chat', async ({ page }) => {
 	// expect page has header Chat
 	await expect(page.getByRole('heading', { name: 'Chat' })).toBeVisible();
@@ -32,4 +31,30 @@ test('chat', async ({ page }) => {
 	await page.getByPlaceholder('Message').fill('My second message');
 	await page.getByRole('button', { name: 'Send' }).click();
 	await expect(page.getByText('My second message')).toBeVisible();
+});
+
+test('chat with another browser', async ({
+	page: alicePage,
+	browser,
+}, testInfo) => {
+	// expect page has header Chat
+	await expect(alicePage.getByRole('heading', { name: 'Chat' })).toBeVisible();
+
+	// send a message
+	await alicePage.getByPlaceholder('Message').fill('Hello world');
+	await alicePage.getByRole('button', { name: 'Send' }).click();
+	await expect(alicePage.getByText('Hello world')).toBeVisible();
+
+	// open a new page with a different context
+	const bobPage = await browser.newContext().then(c => c.newPage());
+
+	// log in as Bob
+	const bobEmail = testInfoToEmail(testInfo);
+	await signupAndLogin(bobPage, bobEmail);
+
+	await expect(bobPage.getByRole('heading', { name: 'Chat' })).toBeVisible();
+
+	// delete Bob
+	await bobPage.close();
+	await deleteUser(bobEmail);
 });
